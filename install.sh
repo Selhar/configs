83da937709e8e52f62a1ac578951c8cb62708691
Selhar <selhar@protonmail.com>
2019-02-28 20:07:02 -0300

    Configures polybar

--- install.sh
+++ install.sh
@@ -7,4 +7,7 @@ fi
 cp ./Xresources $HOME/.Xresources
 cp ./compton.conf $HOME/.config/
 cp ./i3.conf $HOME/.config/i3/config
+cp ./polybar/launch.sh $HOME/.config/polybar/launch.sh
+cp ./polybar/config $HOME/.config/polybar/config
+
 xrdb $HOME/.Xresources
