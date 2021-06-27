5bd3e4637ca81b10b6804cb269ec0830db9364d4
selhar <selhar@protonmail.com>
2021-06-27 17:55:46 -0300

    stable config

--- sway/install.sh
+++ sway/install.sh
@@ -3,7 +3,8 @@ config_folder=/home/$USER/.config/sway
 if [ ! -d $config_folder/settings ]; then
   mkdir $config_folder/settings;
 fi
-
+cp ./definitions $config_folder/definitions
+cp ./config $config_folder/config
 cp -r ./settings/* $config_folder/settings/
 cp ./system/wallpaper.jpg /home/selhar/Documents/System/wallpaper.jpg
 
